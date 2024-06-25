88642a6ef93cfacaba7039d84539d3e4e7c80a84
Henrique Romero <luishenriquebuenoromero@gmail.com>
2024-06-25 20:45:32 +0000

    add other page (miau)

--- Trabalho/components/sobre.tsx
+++ Trabalho/components/sobre.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, ImageBackground } from 'react-native';
 import Home from './home';
-
+import Map from "../assets/images/map.png"
 
 export default function App(){
     const [page, setPage] = React.useState('sobre');
@@ -10,11 +10,23 @@ export default function App(){
         if (page === 'sobre') {
             return (
             <View style={styles.container}>
-                <Text style={styles.title}>Página Sobre</Text>
-                <TouchableOpacity style={styles.button} onPress={() => setPage('home')}>
-                    <Text style={styles.buttonText}>Clique aqui</Text>
+                <ImageBackground source={Map} style={styles.image}>
+                    <TouchableOpacity style={styles.button} onPress={() => setPage('home')}>
+                        <Text style={styles.buttonText}>Back</Text>
                     </TouchableOpacity>
+
+                    <View style={styles.box}>
+                        <Text style={styles.item}>Latitude e Longitude</Text>
+                        <Text style={styles.subtext}>-22.790351711148155</Text>
+                        <Text style={styles.subtext}>-50.207837376186625</Text>
+                        <Text style={styles.item}>Localização Atual</Text>
+                        <Text style={styles.subtext}>Escola Técnica Estadual Professor Mário Antônio Verza, Palmital - SP</Text>
+                        <TouchableOpacity style={styles.buttonGetLocation} onPress={() => setPage('home')}>
+                            <Text style={styles.buttonText}>Obter Localização</Text>
+                        </TouchableOpacity>
                     </View>
+                </ImageBackground>
+            </View>
 );
         } else if (page === 'home') {
           return <Home />;
@@ -29,7 +41,8 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#fff',
+        backgroundColor: '#DFE0E2',
+        width: 430
     },
     title: {
         fontSize: 24,
@@ -38,12 +51,49 @@ const styles = StyleSheet.create({
         marginBottom: 20,
     },
     button: {
-        backgroundColor: '#007BFF',
+        backgroundColor: '#D4D4D4',
         padding: 10,
         borderRadius: 5,
+        width: 55,
+        height: 35
+    },
+    buttonGetLocation: {
+        backgroundColor: '#000',
+        padding: 10,
+        borderRadius: 5,
+        width: 150,
+        height: 35
     },
     buttonText: {
         color: '#FFFFFF',
         fontSize: 16,
-},
+    },
+    image: {
+        width: 370,
+        height: 700,
+        display: "flex",
+        justifyContent: 'space-between',
+        borderRadius: 12,
+        padding: 12
+    },
+    box: {
+        backgroundColor: "#333333",
+        width: "100%",
+        display:"flex",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 25,
+        borderRadius: 20
+    },
+    subtext: {
+        color: "white",
+        fontWeight: "bold",
+        marginBottom: 15,
+        textAlign: "center"
+    },
+    item: {
+        marginBottom: 15,
+        color: "#888787",
+        opacity: 80
+    }
 });
